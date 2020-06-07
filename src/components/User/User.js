1ff7286a48d403ac2ca7513dc68befc9ae3b5825
Nishant garg <iamnishantgarg@gmail.com>
2020-06-07 16:07:18 +0530

    user display complete

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -3,10 +3,10 @@ import classes from "./User.module.css";
 export default function User(props) {
   return (
     <tr className={classes.User}>
-      <td>props.user._id</td>
-      <td>props.user.name</td>
-      <td>props.user.age</td>
-      <td>props.user.sex</td>
+      <td>{props.user._id}</td>
+      <td>{props.user.name}</td>
+      <td>{props.user.age}</td>
+      <td>{props.user.sex}</td>
     </tr>
   );
 }
--- src/components/Userbox/Userbox.js
+++ src/components/Userbox/Userbox.js
@@ -1,30 +1,37 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import "./Userbox.css";
 import User from "../User/User";
+
 import * as actions from "../../store/actions";
 class Userbox extends Component {
   componentDidMount() {
     this.props.getUsers();
   }
   render() {
-    // let users = this.props.users.map((user) => {
-    //   return <User key={user._id} user={user} />;
-    // });
-    let users = "hello";
-    return (
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>User Id</th>
-              <th>Name</th>
-              <th>Age</th>
-              <th>Sex</th>
-            </tr>
-          </thead>
-        </table>
-      </div>
-    );
+    let display = <p>Loading</p>;
+    if (this.props.users) {
+      let users = this.props.users.map((user) => {
+        return <User key={user._id} user={user} />;
+      });
+      display = (
+        <div>
+          <table className="Table">
+            <thead>
+              <tr>
+                <th>User Id</th>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Sex</th>
+              </tr>
+            </thead>
+            <tbody>{users}</tbody>
+          </table>
+        </div>
+      );
+    }
+    // let users = "hello";
+    return <div className="Userbox">{display}</div>;
   }
 }
 
